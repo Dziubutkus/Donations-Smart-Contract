ae1f69a7c8203025a906f3eecf4b482d88b0650f
Dziugas Butkus <dziubutkus@gmail.com>
2018-01-20 23:46:19 -0700

    Added comments. Created changeDonation() function, didn't test.

--- contracts/Donations.sol
+++ contracts/Donations.sol
@@ -1,27 +1,54 @@
 pragma solidity ^0.4.18;
 
+import "./ConvertLib.sol";
+
+/*
+*   Author:         Dziugas Butkus
+*   Description:    DAPP that lets contract's owner to create donation events. Everyone can donate ether to one pool until deadline.
+*                   After deadline, raised funds are transfered to organization and contract's owner has to create a new donation event.
+*/
+
 contract Donations {
 
     address public owner; // Contract creator's address
     address public contractsAddress; // Contract's address
+    address public organizationAddress; // Address to donate to
     uint public fundingGoal = 1; // Funding goal value
     uint public durationInMinutes = 1; // Duration for each donation
     uint public deadline; // Donation stops at deadline
-    bool public donationClosed = false;
-    bool public fundingGoalReached = false;
+    bool public donationClosed = true;
+    bool public fundingGoalReached = true;
     
     mapping (address => uint) balanceOf;
 
     event SendRaisedDonation(address donationTo, uint totalAmountRaised);
     event Donate(address sender, address receiver, uint256 amountDonated);
+    event ChangeDonation(address beneficiary, uint fundingGoal, uint durationInMinutes);
     
-    /*
-    *   Give access to functions after deadline
+    /** 
+    *   Things to pay attention to:
+    *   Add states
+    *   Fix modifiers
+    *   Fix security (require(), ifs())
     */
-    modifier afterDeadLine {
+
+    // Give access to functions after deadline
+    modifier afterDeadline {
         if (now >= deadline) {
             _;
         }
+    }
+
+    // Give access to functions during the donation event
+    modifier duringDeadline {
+        if (now < deadline) {
+            _;
+        }
+    }
+    // Make functions only accessable by the contract's owner
+    modifier onlyOwner { 
+        require(msg.sender == owner);  
+        _; 
     }
     
     /*
@@ -30,15 +57,41 @@ contract Donations {
     function Donations() public {
 		owner = msg.sender;
 		contractsAddress = this;
-		fundingGoal = fundingGoal * 1 ether;
-		deadline = now + (durationInMinutes * 1 minutes);
+		//fundingGoal = fundingGoal * 1 ether;
+		//deadline = now + (durationInMinutes * 1 minutes);
 	}
     
     /*
-    *   Donate to organization
+    *   Create a donation event.
+    *   @params: 
+    *   _organization       - organization's address
+    *   _fundingGoal        - set the intended amount of ether to raise
+    *   _durationInMinutes  - the duration of donation event in minutes
     */
-	function sendDonation(uint _amount) public payable returns(bool success) {
+    function changeDonation(address _organization, uint _fundingGoal, uint _durationInMinutes) onlyOwner afterDeadline public {
+        // Security
+        require(msg.sender == owner);
+        require(donationClosed == true);
+        require(fundingGoalReached == true);
+        // Set variables
+        organizationAddress = _organization;
+        fundingGoal = _fundingGoal * 1 ether;
+        durationInMinutes = _durationInMinutes;
+        // Set deadline
+        deadline = now + (durationInMinutes * 1 minutes);
+        // Set booleans
+        donationClosed = false;
+        fundingGoalReached = false;
+    }
+
+    /*
+    *   Send donation to the pool
+    *   @params:
+    *   _amount - amount of ether to donate
+    */
+	function sendDonation(uint _amount) public payable beforeDeadline returns(bool success) {
 	    require(!donationClosed);
+        require(_amount > 0);
 		if (balanceOf[msg.sender] < _amount) {
     		balanceOf[msg.sender] -= _amount;
             contractsAddress.transfer(_amount);
@@ -50,11 +103,14 @@ contract Donations {
 	
 	/*
 	*   Check if goal is reached. Can only check after deadline = true
+    *   If goal is reached, call transferDonation() to send raised ether to organization
 	*/
-	function checkGoalReached() public payable returns(bool) {
+	function checkGoalReached() public payable afterDeadline returns(bool) {
 	    if (getBalance() >= fundingGoal || now >= deadline) {
+            // Stop donation proccess
 	        fundingGoalReached = true;
 	        donationClosed = true;
+            // Transfer raised ether to organization
 	        transferDonation(getBalance());
 	        SendRaisedDonation(contractsAddress, getBalance());
 	        return true;
@@ -62,24 +118,34 @@ contract Donations {
 	    return false;
 	}
 	
-	function transferDonation(uint _amount) public payable {
+    /*
+    *   Transfer raised donation to organization after deadline
+    *   @params:
+    *   _amount - amount of ether to send to organization. (_amount comes from getBalance())
+    */
+	function transferDonation(uint _amount) public payable afterDeadline {
 	    owner.transfer(_amount);
 	}
     
     /*
-    *   Check contract's balance
+    *   Get contract's balance in wei
     */
     function getBalance() public view returns(uint) {
 		return contractsAddress.balance;
 	}
 
+    /*
+    *   Get contract's balance in ether with 2 decimal points
+    */
+    function getBalanceInEth() public view returns(uint) {
+		return ConvertLib.convert(getBalance(), 2);
+	}
 
     /*
     *   Delete this smart contract from blockchain
     */
-    function kill() public {
+    function kill() public onlyOwner {
         if (msg.sender == owner) {
-
             selfdestruct(owner);
         }
     }
